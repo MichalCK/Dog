9f867ef8704aed95da8967e33138e89867074f17
Michael Woo <michaelwoo@thecoursekey.com>
2020-05-10 18:23:45 -0700

    Initial Commit

---
+++ Dog01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dog01
+//
+//  Created by Michael Woo on 5/10/20.
+//  Copyright © 2020 CourseKey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
